3d69b258cedf7f4ba520dc059e4426f031d4fc89
din94 <macho_sudin@hotmail.com>
2020-09-12 15:03:39 +0800

    modified signup.inc.php file

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -43,15 +43,39 @@ if(isset($_POST['signup-submit'])){
             mysqli_stmt_execute($stmt);
             mysqli_stmt_store_result($stmt);
             
-            $resultCheck = mysqli_stmt_num_row($stmt);
+            $resultCheck = mysqli_stmt_num_rows($stmt);
             
             if($resultCheck > 0){
                 header("Location: ../signup.php?error=usertaken&mail=" . $email);
                 exit();
             }
+
+            else{
+                $sql = "INSERT INTO users(uidUsers, emailUsers, pwdUsers) VALUES(?, ?, ?)";
+                $stmt = mysqli_stmt_init($conn);
+                if(!mysqli_stmt_prepare($stmt, $sql)){
+                    header("Location: ../signup.php?error=sqlerror");
+                    exit();
+                }
+                else{
+                    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+                    mysqli_stmt_bind_param($stmt, "sss", $userName, $email, $hashedPassword);
+                    mysqli_stmt_execute($stmt);
+                    header("Location: ../signup.php?signup=success");
+                    exit();
+                }
+            }
         }
 
     }
 
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
 
 }
+else{
+    header("Location: ../signup.php");
+    exit();
+}
+
