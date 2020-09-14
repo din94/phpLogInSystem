5b24fc54e13833e626c3f8f3ab2fcfe72651cf64
din94 <macho_sudin@hotmail.com>
2020-09-14 12:23:58 +0800

    modified signup file and create logout.inc file

--- header.php
+++ header.php
@@ -24,16 +24,21 @@
                 <li><a href="#">About Me</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
+            </nav>
             <div>
-                <form action="includes/login.inc.php" method="post">
+            <?php
+                if(isset($_SESSION['userId'])){
+                    echo '<form action="includes/logout.inc.php" method="post">
+                        <button type="submit" name="logout-submit">Logout</button>
+                    </form>';
+                } else{
+                    echo '<form action="includes/login.inc.php" method="post">
                     <input type="text" name="mailuid" placeholder="Username/E-mail...">
                     <input type="password" name="pwd" placeholder="Password...">
                     <button type="submit" name="login-submit">Login</button>
                 </form>
-                <a href="signup.php">SignUp</a>
-                <form action="includes/logout.inc.php" method="post">
-                    <button type="submit" name="logout-submit">Logout</button>
-                </form>            
+                <a href="signup.php">SignUp</a>';
+                }
+            ?>        
             </div>
-        </nav>
     </header>
--- signup.php
+++ signup.php
@@ -6,6 +6,30 @@
         <div class="wrapper-main">
             <section class="section-default">
                 <h1>SignUp</h1>
+                <?php
+                    if(isset($_GET['error'])){
+                        if($_GET['error'] == "emptyfields"){
+                            echo '<p class="signuperror">Fill in all fields!</p>';
+                        }
+                        elseif($_GET['error'] == "invaliduidmail"){
+                            echo '<p class="signuperror">Invalid Username and E-mail!</p>';
+                        }
+                        elseif($_GET['error'] == "invaliduid"){
+                            echo '<p class="signuperror">Invalid Username!</p>';
+                        } elseif($_GET['error'] == "invalidmail"){
+                            echo '<p class="signuperror">Invalid E-mail!</p>';
+                        } elseif($_GET['error'] == "passwordcheck"){
+                            echo '<p class="signuperror">Your passwords do not match!</p>';
+                        } elseif($_GET['error'] == "usertaken"){
+                            echo '<p class="signuperror">Username is already taken!</p>';
+                        }
+                    } elseif(isset($_GET['signup'])){
+                        if($_GET['signup'] == "success"){
+                            echo '<p class="signupsuccess">Signup successful!</p>';
+                        }                        
+                    } 
+                    
+                ?>
                 <form class="form-signup" action="includes/signup.inc.php" method="post">
                     <input type="text" name="uid" placeholder="Username">
                     <input type="text" name="mail" placeholder="E-mail">
